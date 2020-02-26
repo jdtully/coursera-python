65011c0e776bbbe0b164a2979a25d28301076383
Jeff <jefftully@gmail.com>
2020-02-26 08:35:10 -0500

    CATCHING UP

---
+++ EMAILS.py
@@ -0,0 +1,6 @@
+Hi there,
+I saw your opportunity listed on  INDEED.  I am interested  in  any IT/support based positions  that  might be  available.   I have  a LONG  history of IT  and  customer  service.  My intention is  to treat  callers   to the IT  department  as  customers.  I am currently working on getting some  certs  to prove my  Experience.
+
+Please  send me an  email  to jefftully@gmail.com   and  we can  get together  on a time and  date for a phonecall!
+
+Thanks for your time
